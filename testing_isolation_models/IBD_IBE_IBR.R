766d5b721613eb9809842e034c27806ee66cced1
xljiao <55725822+xljiao@users.noreply.github.com>
2023-11-09 17:07:44 +0800

    Update IBD_IBE_IBR.R

--- testing_isolation_models/IBD_IBE_IBR.R
+++ testing_isolation_models/IBD_IBE_IBR.R
@@ -1,10 +1,10 @@
 library(geosphere)
 library(Cairo)
-setwd("C:/Users/jiao/OneDrive/leiothrix lutea/paper1/IBD_IBE_IBR")
-setwd("C:/Users/jiaox/Desktop/IBD_IBE_IBR")
+library(sp)
+library(raster)
 
-#calculate the distance of two sites using geographical coordinates
-#https://blog.csdn.net/weixin_34144848/article/details/92013232
+# calculate the distance of two sites using geographical coordinates
+
 location<-read.csv("location.csv",header = TRUE)
 colnames(location)[1]<-"ID"
 muer.lonlat = cbind(location$log, location$lat)
@@ -13,14 +13,10 @@ rownames(muer.dists) = location$ID;
 colnames(muer.dists) = location$ID;
 write.table(muer.dists, 'matrix_distance.csv', quote=FALSE, sep=",");
 
-#extract bioclimatic variables of each sites
-library(sp)
-library(raster)
-help("rasterImage")
+# extract bioclimatic variables of each sites
 LonLatData <- location[,c(2,3)]
 #input bioclimatic variables
-files <- list.files("C:/Users/jiao/Desktop/maxent_new/current_bio_30s",pattern='asc',
-                    full.names=TRUE)
+files <- list.files("current_bio_30s",pattern='asc',full.names=TRUE)
 Grids <- raster::stack(files)
 ID<-location[,c(1)]
 VariablesAtlocations <- raster::extract(Grids,LonLatData)
@@ -53,3 +49,17 @@ write.table(edist_pc1, 'matrix_bio_pc1.csv', quote=FALSE, sep=",")
 pc2 <- read.csv(file="pca2_scores.csv",header=T,row.names=1)
 edist_pc2 <- as.matrix(dist(pc2))
 write.table(edist_pc2, 'matrix_bio_pc2.csv', quote=FALSE, sep=",")
+
+### create sampling sites raster, the grid size needs to be the same as the resistance surface grid size
+testa <- raster("leiothrixlutea_avg.asc")  # resistance surface raster layer
+k <- read.csv("location.csv") #location
+cor <- k[,c(2,3)] #extract the geographical coordinates of sampling sites
+num <- cellFromXY(testa,cor)  #Find the grid number of the coordinate point
+a <- as.data.frame(testa)  # Assign the drag surface raster file to a
+a$leiothrixlutea_avg <- c(rep(-9999,nrow(a))) # test is the column name. The name is based on the file name. The test column (only this column) is changed to -9999
+a[num,1] <- k$value  #Assigns the grid number of the coordinate point to the number set for the site-point in the coordinate point file
+values(testa) <- a$leiothrixlutea_avg #Assigns the modified raster file value to the original raster file
+plot(testa) # can look directly at the current value distribution of the grid
+writeRaster(testa,"location_newn.asc") #Export to raster file
+
+
