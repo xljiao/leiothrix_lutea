2e00acec16571d95b2df05f7a1bb7360e19ca4c4
xljiao <55725822+xljiao@users.noreply.github.com>
2023-11-10 10:17:17 +0800

    Create eems.sh

---
+++ spatial_connection/eems.sh
@@ -0,0 +1,10 @@
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain1.ini --seed 123 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain2.ini --seed 234 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain3.ini --seed 345 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain4.ini --seed 456 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain5.ini --seed 567 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain6.ini --seed 678 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain7.ini --seed 789 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain8.ini --seed 890 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain9.ini --seed 901 
+/home/jiaoxiaolu_org/software/eems/runeems_snps/src/runeems_snps --params params-chain10.ini --seed 012 
